3d7ede2f5cb4342875a83792748b7558901d3244
wuhaostudy1992 <wuhaostudy1992@gmail.com>
2018-04-25 12:10:34 -0700

    update

---
+++ c/client.c
@@ -0,0 +1,92 @@
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+void Cipher(char *message, char *keyPath);
+
+int main(int argc, char *argv[])
+{
+    if(argc < 4)
+    {
+        printf("Not enough parameters.\n");
+    }
+    
+    int port = atoi(argv[1]);
+    char *domainName = argv[2];
+    char *keyPath = argv[3];
+    char message[80] = {0};
+
+    
+
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+      
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    
+    while(1)
+    {
+        printf("Enter the email address: ");
+        //scanf("%[^\n]", message);
+        scanf("%s", message);
+        //printf("The email you entered is: %s\n", message);
+        
+        Cipher(message, keyPath);
+        //printf("After encryption: %s, %ld\n", result, strlen(result));
+
+        send(sock, message, strlen(message), 0);
+        //printf("Hello message sent\n");
+        memset(message,0,sizeof(message));
+        memset(buffer,0,sizeof(buffer));
+        valread = read(sock , buffer, 1024);
+        printf("%s\n",buffer);
+    }
+    return 0;
+}
+
+void Cipher(char *message, char *keyPath)
+{
+    //Read the key file
+    FILE *fp = NULL;
+    char buff[25];
+    fp = fopen(keyPath, "r");
+    fscanf(fp, "%s", buff);
+    fclose(fp);
+    //printf("Content of keyfile is: %s\n", buff );
+    
+    //encryption
+    int i = 0;
+    while(message[i] != '\0')
+    {
+        message[i] = message[i] ^ buff[i];
+        i++;
+    }
+    //printf("After encryption: %s\n", message);
+}
---
+++ c/server.c
@@ -0,0 +1,121 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PORT 8080
+
+void Decipher(char *message, char *keyPath);
+char* Name(char* message);
+int Compare(char* left, char*right);
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+      
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+      
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+      
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    
+    while(1)
+    {
+        memset(buffer,0,sizeof(buffer));
+        valread = read(new_socket , buffer, 1024);
+        //printf("%s, %ld\n",buffer, strlen(buffer));
+        Decipher(buffer, "mykey.txt");
+        //printf("%s, %ld\n",buffer, strlen(buffer));
+        char *name = Name(buffer);
+        send(new_socket , name , strlen(name) , 0);
+    }
+    return 0;
+}
+
+void Decipher(char *message, char *keyPath)
+{
+    //Read the key file
+    FILE *fp = NULL;
+    char buff[25];
+    fp = fopen(keyPath, "r");
+    fscanf(fp, "%s", buff);
+    fclose(fp);
+    //printf("Content of keyfile is: %s\n", buff );
+    
+    //dencryption
+    int i = 0;
+    while(message[i] != '\0')
+    {
+        message[i] = message[i] ^ buff[i];
+        i++;
+    }
+    //printf("After encryption: %s\n", message);
+}
+
+char* Name(char* message)
+{
+    if(Compare(message, "harris@ics.uci.edu"))
+        return "Lan G. Harris";
+    else if(Compare(message, "joe@cnn.com"))
+        return "Joe Smith";
+    else if(Compare(message, "jane@slashdot.org"))
+        return "Jane Smith";
+    else if(Compare(message, "test"))
+        return "This is for testing";
+    else if(Compare(message, "t"))
+        return "Th";
+    else
+        return "Unknown";
+}
+
+int Compare(char* left, char*right)
+{
+    int length = strlen(left);
+    if(length != strlen(right))
+    {
+        return 0;
+    }
+    int i = 0;
+    while(i < length)
+    {
+        if(left[i] != right[i])
+            return 0;
+        i++;
+    }
+    return 1;
+}
